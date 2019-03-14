36f7c463f47ed128b4b68184716d9f27f1c322bb
Ivan Vegner <ivanvegner@gmail.com>
2019-03-14 03:02:17 -0400

    Add training, doesn't work yet

--- DifferentiableSNN.py
+++ DifferentiableSNN.py
@@ -1,10 +1,13 @@
 import tensorflow as tf
 import numpy as np
-from collections import deque
+
+# from collections import deque
+from itertools import zip_longest
 
 tf.enable_eager_execution()
 
 ACTIVATION_HISTORY_LEN = 10
+LEARNING_RATE = 0.01
 
 # shape = (width, height, depth)
 # n_in, n_out = number of inputs/outputs
@@ -15,20 +18,21 @@ class DSNN:
         self.multipliers_net = tf.get_variable(
             "multipliers", shape=(1, *self.shape, 1), initializer=tf.random_normal_initializer()
         )
+        self.activations_net = tf.Variable(tf.zeros_like(self.multipliers_net))
 
-        self.reset_activations()
         # first row for inputs, last row for outputs. TODO: input and output on different heights.
         input_cell_gap = shape[0] // (n_in + 1)
         self.input_indices = [
-            (input_cell_gap * (i + 1), 0) + (0,) * (self.n_dims - 2) for i in range(n_in)
+            (0, input_cell_gap * (i + 1), 0) + (0,) * (self.n_dims - 2) + (0,) for i in range(n_in)
         ]
         output_cell_gap = shape[0] // (n_out + 1)
         self.output_indices = [
-            (output_cell_gap * (i + 1), -1) + (0,) * (self.n_dims - 2) for i in range(n_out)
+            (0, output_cell_gap * (i + 1), self.shape[1] - 1) + (0,) * (self.n_dims - 2) + (0,)
+            for i in range(n_out)
         ]
 
         # build the N-D cross-shaped convolutional filter for the activations net
-        # For 2D, it looks like
+        # e.g. for 2D, it looks like
         # [[0,1,0],
         #  [1,1,1],
         #  [0,1,0]]
@@ -38,27 +42,79 @@ class DSNN:
         )
 
     def step(self, inputs=None):
-        # first, save the current step into the history
-        self.activation_history.extend(tf.identity(self.activations_net))
+        # # first, save the current step into the history
+        # self.activation_history.extend(tf.identity(self.activations_net))
         if inputs:
             # put new inputs into the input neurons
-            for i, input_idx in enumerate(self.input_indices):
-                tf.assign(self.activations_net[(0, *input_idx, 0)], inputs[i])
+            self.activations_net = tf.scatter_nd_update(self.activations_net, self.input_indices, inputs)
+            # for i, input_idx in enumerate(self.input_indices):
+            #     tf.assign(self.activations_net[input_idx], inputs[i])
 
         print("Start of step:")
         self.print_net()
-        # run update steps
 
-        self.activations_net = tf.nn.convolution(self.activations_net, self.filter, "SAME")
+        # add up neighbors of every cell
+        # this is equivalent to a convolution with a cross-shaped additive filter
+        self.activations_net.assign(tf.nn.convolution(self.activations_net, self.filter, "SAME"))
+
+        # apply activation/threshold function
+        # TODO
+        # multiply by the multipliers to get the final activations
+        self.activations_net.assign(self.multipliers_net * self.activations_net)
         print("End of step:")
         self.print_net()
+        return tf.gather_nd(self.activations_net, self.output_indices)
+
+    def train(self, inputs, targets, epochs=10, step_to_nonzero=True):
+        """
+        `inputs`: iterable of inputs
+
+        `targets`: iterable of corresponding labels
+
+        `step_to_nonzero`: whether to backprop every step, or just the ones where the output is nonzero.
+        If True (default), the training will feed in the input, then call `step()` until the output
+        is nonzero, then backprop. If it is False, it will backprop every step - this probably means that you need to
+        make input-output pairs for EVERY timestep.
+        """
+        try:
+            if len(inputs) != len(targets):
+                raise ValueError("X and Y have different length!")
+        except TypeError:
+            # no __len__ on x or y
+            pass
+
+        for epoch in range(epochs):
+            sentinel = object()
+            aggregate_loss = 0
+            # Iterate while ensuring that x and y have the same lengths
+            for (x, y) in zip_longest(inputs, targets, fillvalue=sentinel):
+                if sentinel in (x, y):
+                    raise ValueError("X and Y have different lengths!")
+
+                # enter auto-grad context
+                x = tf.constant(x, dtype="float32")
+                with tf.GradientTape() as tape:
+                    tape.watch(x)
+                    out = self.step(x)
+                    if step_to_nonzero:
+                        while out.numpy() == 0:
+                            out = self.step()
+                    print("FINAL OUT", out)
+                    loss = tf.reduce_mean(tf.square(y - out))
+                    print("LOSS", loss)
+                    aggregate_loss += loss
+                de_dm = tape.gradient(loss, self.multipliers_net)
+                print("GRADIENT", de_dm, sep="\n")
+                # self.optimizer.apply_gradients([(de_dm, self.multipliers_net)])
+                self.multipliers_net = LEARNING_RATE * de_dm
+            print("Epoch {} over - loss: {}".format(epoch, aggregate_loss))
 
     def reset_activations(self):
         """Set all activations to 0, to "clear the net" for a fresh run of inputs
         Useful for when you don't want to carry over state from one input to the next
         """
-        self.activations_net = tf.Variable(tf.zeros(tf.shape(self.multipliers_net)))
-        self.activation_history = deque([], ACTIVATION_HISTORY_LEN)
+        self.activations_net.assign(tf.zeros_like(self.multipliers_net))
+        # self.activation_history = deque([], ACTIVATION_HISTORY_LEN)
 
     def _build_filter(self, dims, elem=None):
         if elem is None:
@@ -72,12 +128,19 @@ class DSNN:
         return elem
 
     def print_net(self):
-        print(tf.squeeze(self.activations_net).numpy())
+        print("Multipliers", tf.squeeze(self.multipliers_net).numpy(), sep="\n")
+        print("Activations", tf.squeeze(self.activations_net).numpy(), sep="\n")
+        print("\n")
 
 
 net = DSNN((3, 3), 1, 1)
 print("NET SHAPE", net.activations_net.shape)
 print("FILTER SHAPE", net.filter.shape)
 
-net.step([5])
-net.step()
+# print("Out: ", net.step([5]))
+# print("Out: ", net.step())
+
+inputs = [[1], [2], [3], [4]]
+targets = [[2], [3], [4], [5]]
+
+net.train(inputs, targets)
